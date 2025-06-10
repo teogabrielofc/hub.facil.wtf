da6647b5f0929e42b2b4e73107a28b84bab638f8
teoga <teoga@TEOGABRIEL>
2025-06-09 22:04:04 -0300

    Esqueci de modificar o site e o loader

--- loader.lua
+++ loader.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 8200787440 then
   loadstring(game:HttpGet("https://hub.facil.wtf/Eat-Blobs-Simulator.lua",true))()
+elseif game.PlaceId == 111989938562194 then
+  loadstring(game:HttpGet("https://hub.facil.wtf/BrainRot-Evolution.lua", true))()
 else
     
     game.StarterGui:SetCore("SendNotification", {
