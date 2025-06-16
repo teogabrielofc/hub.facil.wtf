c2581b7e329dccd8a77ccf02f60ed7b96d5018f8
teoga <teoga@TEOGABRIEL>
2025-06-16 14:50:09 -0300

    Errei o loader

--- loader.lua
+++ loader.lua
@@ -4,7 +4,7 @@ elseif game.PlaceId == 111989938562194 then
   loadstring(game:HttpGet("https://hub.facil.wtf/BrainRot-Evolution.lua", true))()
 elseif game.PlaceId == 101354156600579 then
   loadstring(game:HttpGet("https://hub.facil.wtf/Steal-a-Character.lua", true)) ()
-elseif game.PlaceId == 82653141945191 then
+elseif game.PlaceId == 123382689033007 then
   loadstring(game:HttpGet("https://hub.facil.wtf/Steal-a-Brainrot-MODDED.lua", true)) ()
 else
     
